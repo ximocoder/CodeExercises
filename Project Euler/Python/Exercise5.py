01b285a5a68ae139add3a86e7135556e8080f013
ximocoder <ximo.coder@gmail.com>
2016-08-11 10:05:47 +0200

    Solved exercise 5 of Euler.

--- Project Euler/Python/Exercise5.py
+++ Project Euler/Python/Exercise5.py
@@ -5,3 +5,22 @@
  #  What is the smallest number that is evenly divisible by all of the numbers
  #  from 1 to 20?
 
+isOk = True
+num = 0
+while num < 100000000000:
+    num += 1
+
+    isOk = True
+    for i in range(1, 21):
+        if num % i != 0:
+            isOk = False
+
+    if isOk:
+        print(num)
+        break
+
+#result> 232792560
+#Congratulations, the answer you gave to problem 5 is correct.
+#You are the 319467th person to have solved this problem.
+
+
