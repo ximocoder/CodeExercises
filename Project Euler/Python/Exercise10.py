6e0e53766bfcf69574336cccf28d563d61d48aa1
ximocoder <ximo.coder@gmail.com>
2016-08-16 12:22:14 +0200

    Exercise 10 solved

--- Project Euler/Python/Exercise10.py
+++ Project Euler/Python/Exercise10.py
@@ -3,11 +3,35 @@
 
 sumprime = 0
 
-for num in range(1, 2000000):
-    prime = True
-    for i in range(2, num):
-        if num % i == 0:
-            prime = False
+# for num in range(1, 2000000):
+#     prime = True
+#     for i in range(2, num):
+#         if num % i == 0:
+#             prime = False
+#
+#     if prime:
+#         sumprime += num
 
-    if prime:
-        sumprime += num
+def primes_sieve(limit):
+    limitn = limit+1
+    not_prime = [False] * limitn
+    primes = []
+    sum = 0
+
+    for i in range(2, limitn):
+        if not_prime[i]:
+            continue
+        for f in range(i*2, limitn, i):
+            not_prime[f] = True
+
+        primes.append(i)
+        sum += i
+    return sum
+
+
+sumprime = primes_sieve(2000000)
+print(sumprime)
+
+# result: 142913828922
+# Congratulations, the answer you gave to problem 10 is correct.
+# You are the 217767th person to have solved this problem.
