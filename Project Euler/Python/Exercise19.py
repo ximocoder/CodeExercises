92477ed911e73032abce702b3c2c97f865010d39
ximo <ximo.coder@gmail.com>
2016-09-16 20:33:54 +0200

    solution created for exercise 19 of project euler

--- Project Euler/Python/Exercise19.py
+++ Project Euler/Python/Exercise19.py
@@ -18,3 +18,24 @@
 #
 #    How many Sundays fell on the first of the month during the twentieth
 #    century (1 Jan 1901 to 31 Dec 2000)?
+#
+# MORE INFO
+# Return the day of the week as an integer, where Monday is 0 and Sunday is 6.
+# >>> import datetime
+# >>> datetime.datetime.today()
+# datetime.datetime(2012, 3, 23, 23, 24, 55, 173504)
+# >>> datetime.datetime.today().weekday()
+
+import datetime
+
+date = datetime.date(1901, 1, 1)
+sumsundays = 0
+
+while date.year < 2001:
+    date += datetime.timedelta(days=10)
+    if date.weekday() == 6:
+        sumsundays += 1
+
+print(sumsundays)
+
+
