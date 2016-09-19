5cb2aae8ed004ea494f4d2c73e9644343d29447b
ximocoder <ximo.coder@gmail.com>
2016-09-19 10:24:27 +0200

    Coded exercise 20 of euler

--- Project Euler/Python/Exercise20.py
+++ Project Euler/Python/Exercise20.py
@@ -6,3 +6,17 @@
 #
 #    Find the sum of the digits in the number 100!
 
+
+res = 100
+
+for n in range(100, 1,  -1):
+    res *= n - 1
+
+
+print(res)
+
+sum = 0
+for i in str(res):
+    sum += int(i)
+
+print(sum)
