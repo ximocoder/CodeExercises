44692001b677cfeb4b7a5048c41da28ede19b5a9
ximo <ximo.coder@gmail.com>
2016-10-25 17:42:30 +0200

    added matrix on exercise 28

--- Project Euler/Python/Exercise28.py
+++ Project Euler/Python/Exercise28.py
@@ -15,3 +15,11 @@
 #
 #    What is the sum of both diagonals in a 1001 by 1001 spiral formed in the
 #    same way?
+
+mat = """ 21 22 23 24 25
+20  7  8  9 10
+19  6  1  2 11
+18  5  4  3 12
+17 16 15 14 13 """
+
+print(mat)
