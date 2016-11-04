bf49d17f7572fcbe56554fe0309c8338a63701c7
ximocoder <ximo.coder@gmail.com>
2016-11-04 14:31:52 +0100

    some changes on the rust test helloworld

--- Rust/Exercises/rust_helloworld/src/main.rs
+++ Rust/Exercises/rust_helloworld/src/main.rs
@@ -4,14 +4,37 @@
 
 // This code is editable, feel free to hack it!
 // You can always return to the original code by clicking the "Reset" button ->
+use std::io;
+use std::io::prelude::*;
+
+
+  fn pause() {
+    let mut stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    // We want the cursor to stay at the end of the line, so we print without a newline and flush manually.
+    write!(stdout, "Press any key to continue...").unwrap();
+    stdout.flush().unwrap();
+
+    // Read a single byte and discard
+    let _ = stdin.read(&mut [0u8]).unwrap();
+    }
 
 // This is the main function
 fn main() {
     // The statements here will be executed when the compiled binary is called
 
+  
+    let _unused_variable = 3u32;
+
+    let noisy_unused_variable = 2u32;
+
     // Print text to the console
     println!("Hello World!");
-    println!("test);
+    println!("test");
+     // Special formatting can be specified after a `:`.
+    println!("{} of {:b} people know binary, the other half don't", _unused_variable, 2);
+    pause();
     
       
 }
