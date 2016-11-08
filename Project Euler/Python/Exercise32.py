5f856233c652a34462d7abbdce434ab58ac624ff
ximo <ximo.coder@gmail.com>
2016-11-08 19:29:30 +0100

    comment on ex 32

--- Project Euler/Python/Exercise32.py
+++ Project Euler/Python/Exercise32.py
@@ -1,12 +1,12 @@
-
-Problem 32
-==========
-
-
-   The product 7254 is unusual, as the identity, 39 * 186 = 7254, containing
-   multiplicand, multiplier, and product is 1 through 9 pandigital.
-
-   Find the sum of all products whose multiplicand/multiplier/product
-   identity can be written as a 1 through 9 pandigital.
-   HINT: Some products can be obtained in more than one way so be sure to
-   only include it once in your sum.
+#
+# Problem 32
+# ==========
+#
+#
+#    The product 7254 is unusual, as the identity, 39 * 186 = 7254, containing
+#    multiplicand, multiplier, and product is 1 through 9 pandigital.
+#
+#    Find the sum of all products whose multiplicand/multiplier/product
+#    identity can be written as a 1 through 9 pandigital.
+#    HINT: Some products can be obtained in more than one way so be sure to
+#    only include it once in your sum.
