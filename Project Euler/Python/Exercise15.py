21d2eef67f5e0c0844f2522dfdcd74caf7099a46
ximo <ximo.coder@gmail.com>
2017-01-02 22:57:22 +0100

    cleaned ex. 15

--- Project Euler/Python/Exercise15.py
+++ Project Euler/Python/Exercise15.py
@@ -7,14 +7,3 @@
 #    (without backtracking) to the bottom right corner.
 #
 #    How many routes are there through a 20 * 20 grid?
-
-<<<<<<< HEAD
-<<<<<<< .merge_file_a13748
-=======
-
-
->>>>>>> .merge_file_a10912
-=======
-
-
->>>>>>> e3c4615f1ca0156bfcc50fdcbe8e3798b25c421f
