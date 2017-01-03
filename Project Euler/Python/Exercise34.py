891a4ebb0fd79a904dbfd9b15ce5652410164bc0
ximo <ximo.coder@gmail.com>
2017-01-03 19:50:14 +0100

    cleaned ex34

--- Project Euler/Python/Exercise34.py
+++ Project Euler/Python/Exercise34.py
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< .merge_file_a14716
-Problem 34
-==========
-
-
-   145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
-
-   Find the sum of all numbers which are equal to the sum of the factorial of
-   their digits.
-
-   Note: as 1! = 1 and 2! = 2 are not sums they are not included.
-=======
-=======
->>>>>>> e3c4615f1ca0156bfcc50fdcbe8e3798b25c421f
 # Problem 34
 #==========
 #
@@ -22,9 +7,4 @@ Problem 34
 #  Find the sum of all numbers which are equal to the sum of the factorial of
 #   their digits.
 #
-<<<<<<< HEAD
 #   Note: as 1! = 1 and 2! = 2 are not sums they are not included.
->>>>>>> .merge_file_a13356
-=======
-#   Note: as 1! = 1 and 2! = 2 are not sums they are not included.
->>>>>>> e3c4615f1ca0156bfcc50fdcbe8e3798b25c421f
