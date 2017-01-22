562a5002517af7e97af1faa7b282400f7728aac0
ximo <ximo.coder@gmail.com>
2017-01-22 22:00:10 +0100

    changed text ex. 41

--- Project Euler/Python/Exercise41.py
+++ Project Euler/Python/Exercise41.py
@@ -1,18 +1,11 @@
 
-Problem 40
+
+Problem 41
 ==========
 
 
-   An irrational decimal fraction is created by concatenating the positive
-   integers:
+   We shall say that an n-digit number is pandigital if it makes use of all
+   the digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital
+   and is also prime.
 
-                     0.123456789101112131415161718192021...
-
-   It can be seen that the 12^th digit of the fractional part is 1.
-
-   If d[n] represents the n^th digit of the fractional part, find the value
-   of the following expression.
-
-       d[1] * d[10] * d[100] * d[1000] * d[10000] * d[100000] * d[1000000
-
-   ]
+   What is the largest n-digit pandigital prime that exists?
