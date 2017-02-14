87975d2f1ea73340da2c160bc4eae768b9dba959
ximo <ximo.coder@gmail.com>
2017-02-14 21:35:25 +0100

    added second webapi python

---
+++ Python/WebAPI/home2.py
@@ -0,0 +1,20 @@
+#webapi 
+#http://blog.luisrei.com/articles/flaskrest.html
+
+from flask import Flask, url_for
+app = Flask(__name__)
+
+@app.route('/')
+def api_root():
+    return 'Welcome'
+
+@app.route('/articles')
+def api_articles():
+    return 'List of ' + url_for('api_articles')
+
+@app.route('/articles/<articleid>')
+def api_article(articleid):
+    return 'You are reading ' + articleid
+
+if __name__ == '__main__':
+    app.run()
