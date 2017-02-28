6207ff222e0033f05cdf49e422dd0fd0c4528709
ximo <ximo.coder@gmail.com>
2017-02-28 23:15:38 +0100

    testing rust

--- Rust/Exercises/rust_helloworld/src/main.rs
+++ Rust/Exercises/rust_helloworld/src/main.rs
@@ -31,8 +31,10 @@ fn main() {
 
     let noisy_unused_variable = 2u32;
 
+    let test = "test string";
+
     // Print text to the console
-    println!("Hello World!");
+    println!("Hello World!" + test);
 
     if (true)
     {
@@ -41,6 +43,51 @@ fn main() {
      // Special formatting can be specified after a `:`.
     println!("{} of {:b} people know binary, the other half don't", _unused_variable, 2);
     pause();
+    main2();
     
       
+}
+
+fn main2() {
+    // In general, the `{}` will be automatically replaced with any
+    // arguments. These will be stringified.
+    println!("{} days", 31);
+
+    // Without a suffix, 31 becomes an i32. You can change what type 31 is,
+    // with a suffix.
+
+    // There are various optional patterns this works with. Positional
+    // arguments can be used.
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+
+    // As can named arguments.
+    println!("{subject} {verb} {object}",
+             object="the lazy dog",
+             subject="the quick brown fox",
+             verb="jumps over");
+
+    // Special formatting can be specified after a `:`.
+    println!("{} of {:b} people know binary, the other half don't", 1, 2);
+
+    // You can right-align text with a specified width. This will output
+    // "     1". 5 white spaces and a "1".
+    println!("{number:>width$}", number=1, width=6);
+
+    // You can pad numbers with extra zeroes. This will output "000001".
+    println!("{number:>0width$}", number=1, width=6);
+
+    // It will even check to make sure the correct number of arguments are
+    // used.
+    println!("My name is {0}, {1} {0}", "Bond");
+    // FIXME ^ Add the missing argument: "James"
+
+    // Create a structure which contains an `i32`. Name it `Structure`.
+    #[allow(dead_code)]
+    struct Structure(i32);
+
+    // However, custom types such as this structure require more complicated
+    // handling. This will not work.
+    println!("This struct `{}` won't print...", Structure(3));
+    // FIXME ^ Comment out this line.
+      pause();
 }
