63696de2812a29b826f3982301b6615ff6c9e79a
ximo <ximo.coder@gmail.com>
2017-03-05 20:52:05 +0100

    rust testing

--- Rust/Exercises/rust_test/rust_test/src/main.rs
+++ Rust/Exercises/rust_test/rust_test/src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+use std::io::prelude::*;
+
+fn pause() {
+    let mut stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    // We want the cursor to stay at the end of the line, so we print without a newline and flush manually.
+    write!(stdout, "Press any key to continue...").unwrap();
+    stdout.flush().unwrap();
+
+    // Read a single byte and discard
+    let _ = stdin.read(&mut [0u8]).unwrap();
+    }
+
 fn main() {
 	/*
 	 * The let keyword introduces a local variable.
@@ -32,4 +47,6 @@ fn main() {
 
 	// // {:?} will conveniently print any type
 	println!("what is this thing: {:?}", monster_size);
+
+    pause();
 }
