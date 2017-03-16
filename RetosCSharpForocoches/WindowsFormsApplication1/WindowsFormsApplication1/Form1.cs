a1d2922fd074abe273c1f565b40558f53e470941
ximo <ximo.coder@gmail.com>
2017-03-16 22:37:36 +0100

    modificacion para burlao

--- RetosCSharpForocoches/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ RetosCSharpForocoches/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -21,7 +21,8 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Javanal++");
+            //MessageBox.Show("@burlao me come el nabo");
+            this.label1.Text = "@burlao me come el nabo";
         }
     }
 }
